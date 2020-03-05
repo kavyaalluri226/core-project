0c445fa090824d04e740a0d4e9a7e483af2d6855
kavya <kavyaalluri226@users.noreply.github.com>
2020-03-05 15:49:03 -0500

    Adding courses

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,35 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: ""
+    }
+  };
+
+  handleChange = event => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    alert(this.state.course);
+  };
+
   render() {
-    return <h1>In courses Page</h1>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h1>Subject</h1>
+        <h2>Add subjects</h2>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        ></input>
+        <input type="submit" value="save"></input>
+      </form>
+    );
   }
 }
 
